d5a3bc008c66c9956e745f4c95ae21dd37124b56
NatbooZ <115220196+NatbooZ@users.noreply.github.com>
2022-10-12 21:03:00 +0700

    ส่งไลน์ Noti

---
+++ test-sendline.py
@@ -0,0 +1,11 @@
+
+from songline import Sendline
+
+token = 'VHxzfaxi9e1HD7XTTmbsZT77A9fHBdrFMVCprOsSanB'
+
+messenger = Sendline(token)
+messenger.sendtext('ไปนอนได้')
+#messenger.sticker(28,2)
+#messenger.sendimage('https://www.xn--k3cc7brobq0b3a7a3s.com/wp-content/uploads/2021/02/137570822_1559753457558564_5180043914746750046_o-520x508.jpg')
+
+
