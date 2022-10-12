27b4dcd3d523f2272134ed95f3782a9c218a3be5
NatbooZ <115220196+NatbooZ@users.noreply.github.com>
2022-10-12 21:03:41 +0700

    วาดเต่า

---
+++ Rectangle-turtle.py
@@ -0,0 +1,24 @@
+import sys
+
+print(sys.version)
+
+
+import turtle
+tao = turtle.Pen() #ดึงความสามารถการใช้ปากกา
+tao.shape('turtle') #แปลงร่างเป็นเต่า
+
+def Rectangle():
+    '''ฟังก์ชั่นนี้เอาไว้วาดรูปสี่เหลี่ยม'''
+    for i in range(4):
+        tao.forward(100)
+        tao.left(90)
+
+
+def Go(x,y):
+    tao.penup()
+    tao.goto(x,y)
+    tao.pendown()
+
+
+Go(200,200)
+Rectangle()
